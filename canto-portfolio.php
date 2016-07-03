3fa52e1d0c1990402dd179ae045227b0e1d49ded
Raju Ahmmed <rajuahmmedbd@gmail.com>
2016-07-03 09:45:23 +0600

    init

---
+++ canto-portfolio.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ *  Plugin Name: Canto Portfolio
+ *  Plugin URL: https://www.cantothemes.com
+ *  Description: Canto Clients simple and effective clients shortcode.
+ *  Author: CantoThemes
+ *  Version: 1.0
+ *  Author URI: https://www.cantothemes.com
+ *  Text Domain: canto-portfolio
+ *  License:     GPL-3.0+
+ *  License URI: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
